375307ff4dad72853241875a16b2a2e69f54eb5e
lohith <lohithkumarme15@gmail.com>
2022-06-03 22:54:16 +0530

    first commit

--- clsobj/src/clsobj/Company1.java
+++ clsobj/src/clsobj/Company1.java
@@ -5,7 +5,7 @@ public class Company1 {
 		String name;
 		public static void main(String[] args) {
 		Company1 c1 = new Company1();
-		c1.name = " C2TC ";
+		c1.name = " King ";
 		Company1 c2 = c1;
 		c1 = null;
 		System.out.println(c2.name);
